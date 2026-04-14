2b6e088caef4f32b663d458b9cc805e6e170c800
zainabehajaje <zainabehajaje@email.com>
2026-04-14 12:52:53 +0100

    feat: add user API module
    docs: add API comment
    
    feat: complete user API (list, delete, pagination todo)

---
+++ src/api.py
@@ -0,0 +1,9 @@
+utilisateurs = []
+
+def ajouter_utilisateur(nom, email):
+    utilisateurs.append({"nom": nom, "email": email})
+    return utilisateurs
+# Mise à jour API
+def lister_utilisateurs(): return utilisateurs
+def supprimer_utilisateur(email): pass
+# TODO: add pagination
